1a6db93a26ed329f5737906fc53e6d38041e7cea
3ndoflin3 <kirylbilbao@hotmail.com>
2020-01-17 12:42:54 +0100

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
 
+//Componente principal que se carga al arrancar la app
+/**
+ * SELECTOR: nombre de la etiqueta para inyectar componente en html
+ * TEMPLATEURL: nombre de la vista del html 
+ * styleUrls: estilos para la vista formato scss
+ */
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //Vatiable que se visualiza con {{title}} en el html
   title = 'my-app';
+
+  productos = [{
+		"id": 34,
+		"nombre": "cafe",
+		"gluten": true,
+		"precio": 1.46,
+		"categoria": {
+			"id": 1,
+			"nombre": "bebidas"
+		},
+		"historico": [{
+				"fecha": "01/12/2019",
+				"precio": 1.50
+			},
+			{
+				"fecha": "01/12/2019",
+				"precio": 1.50
+			},
+			{
+				"fecha": "01/12/2019",
+				"precio": 1.50
+			}
+		]
+	},
+	{
+		"id": 34,
+		"nombre": "galletas",
+		"gluten": false,
+		"precio": 1.16,
+		"categoria": {
+			"id": 4,
+			"nombre": "dulces"
+		},
+		"historico": [{
+				"fecha": "07/02/2019",
+				"precio": 4.60
+			},
+			{
+				"fecha": "01/02/2019",
+				"precio": 1.50
+			},
+			{
+				"fecha": "01/12/2019",
+				"precio": 1.50
+			}
+		]
+	},
+	{}
+];
+
+pSeleccionado = this.productos[0];
+
+seleccionarProducto = function (event, producto){
+  console.log('hemos hecho click');
+  this.pSeleccionado = producto;
+};
+
 }
