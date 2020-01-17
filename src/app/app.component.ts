3bfc3ee273e3e7c2fdd01d7bbe121fb2e1a4ca35
3ndoflin3 <kirylbilbao@hotmail.com>
2020-01-17 13:56:26 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,53 +17,28 @@ export class AppComponent {
   //Vatiable que se visualiza con {{title}} en el html
   title = 'my-app';
 
-  productos = [{
-		"id": 34,
-		"nombre": "cafe",
-		"gluten": true,
-		"precio": 1.46,
+  productos = [
+	{
+		"id": 34,"nombre": "cafe","gluten": true,"precio": 1.46,
 		"categoria": {
 			"id": 1,
 			"nombre": "bebidas"
 		},
-		"historico": [{
-				"fecha": "01/12/2019",
-				"precio": 1.50
-			},
-			{
-				"fecha": "01/12/2019",
-				"precio": 1.50
-			},
-			{
-				"fecha": "01/12/2019",
-				"precio": 1.50
-			}
-		]
+		"historico": [
+			{"fecha": "01/12/2019","precio": 1.50},
+			{"fecha": "01/12/2019","precio": 1.50},
+			{"fecha": "01/12/2019","precio": 1.50}]
 	},
+
 	{
-		"id": 34,
-		"nombre": "galletas",
-		"gluten": false,
-		"precio": 1.16,
+		"id": 34,"nombre": "galletas","gluten": false,"precio": 1.16,
 		"categoria": {
 			"id": 4,
-			"nombre": "dulces"
-		},
-		"historico": [{
-				"fecha": "07/02/2019",
-				"precio": 4.60
-			},
-			{
-				"fecha": "01/02/2019",
-				"precio": 1.50
-			},
-			{
-				"fecha": "01/12/2019",
-				"precio": 1.50
-			}
-		]
+			"nombre": "dulces"},
+		"historico": [{"fecha": "07/02/2019","precio": 4.60},
+			{"fecha": "01/02/2019","precio": 1.50},
+			{"fecha": "01/12/2019","precio": 1.50}]
 	},
-	{}
 ];
 
 pSeleccionado = this.productos[0];
@@ -72,5 +47,18 @@ seleccionarProducto = function (event, producto){
   console.log('hemos hecho click');
   this.pSeleccionado = producto;
 };
+
+products = { "products" : [
+	{ "Name": "Cheese", "Price" : 2.50, "Location": "Refrigerated foods"},
+	{ "Name": "Crisps", "Price" : 3, "Location": "the Snack isle"},
+	{ "Name": "Pizza", "Price" : 4, "Location": "Refrigerated foods"},
+	{ "Name": "Chocolate", "Price" : 1.50, "Location": "the Snack isle"},
+	{ "Name": "Self-raising flour", "Price" : 1.50, "Location": "Home baking"},
+	{ "Name": "Ground almonds", "Price" : 3, "Location": "Home baking"}
+  ]};
+
+
+
+
 
 }
--- src/app/componentes/prueba/prueba.component.ts
+++ src/app/componentes/prueba/prueba.component.ts
@@ -19,4 +19,8 @@ export class PruebaComponent implements OnInit {
 
   }
 
+  ngOnClick(){
+    
+  }
+
 }
